592827678d4dbb057ca40dc0b78bbe415e08b52e
Malik <hiraev.github@gmail.com>
2022-09-15 23:29:07 +0300

    Add teamcity settigns

---
+++ .teamcity/settings.kts
@@ -0,0 +1,106 @@
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.projectFeatures.buildReportTab
+import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2020.1"
+
+project {
+    description = "Contains all other projects"
+
+    vcsRoot(HttpsGithubComHiraevPublicTeamcitySettingsGit)
+    cleanup {
+        baseRule {
+            preventDependencyCleanup = false
+        }
+    }
+
+    subProject {
+        id("TheGuardianNews_App")
+        name = "TheGuardianNews App"
+
+        vcsRoot(TheGuardianNewsVcs)
+
+        params {
+            text("env.AAA", "fds")
+            param("env.CCCCC", "true")
+        }
+        buildType {
+            vcs {
+                root(TheGuardianNewsVcs)
+            }
+            id("MyBuild")
+            name = "Just a Testing Build"
+
+
+            steps {
+                gradle {
+                    buildFile = "build.gradle.kts"
+                    enabled = true
+                    name = "prebuild"
+                    id = "23df2280-1679-4eef-af4d-2d5b16f3941c"
+                    tasks = "app:clean"
+
+                    executionMode = BuildStep.ExecutionMode.ALWAYS
+                    gradleParams = "--stacktrace --no-build-cache"
+                }
+                gradle {
+                    buildFile = "build.gradle.kts"
+                    enabled = true
+                    name = "DoesnExistBuild2"
+
+                    id = "23df2280-1679-4e9f-af4d-2d5b16f3941c"
+                    tasks = "doesntExistTask"
+                    executionMode = BuildStep.ExecutionMode.ALWAYS
+                    gradleParams = "--stacktrace --no-build-cache"
+
+                    conditions {
+                        doesNotExist("skipChecks")
+                    }
+                }
+                gradle {
+                    enabled = true
+                    name = "Build"
+                    // buildFile = "build.gradle.kts"
+                    // useGradleWrapper = true
+
+                    tasks = "app:build"
+                    executionMode = BuildStep.ExecutionMode.RUN_ON_SUCCESS
+                }
+            }
+        }
+    }
+}
+
+object HttpsGithubComHiraevPublicTeamcitySettingsGit : GitVcsRoot({
+    name = "TeamCity Settings"
+    url = "https://github.com/Hiraev/public-teamcity-settings.git"
+})
+
+object TheGuardianNewsVcs : GitVcsRoot({
+    name = "TheGuardianNews"
+    url = "https://github.com/Hiraev/TheGuardianNews.git"
+})
--- README.md
+++ README.md
@@ -62,4 +62,31 @@ ssh-keygen -f teamcity_rsa -N passphrase -t rsa -m PEM
 9. В github добавляем ssh ключ скопированный из teamcity_rsa.pub
 10. Создаем пустой проект на github и копируем ссылку на него в формате https://github.com/Hiraev/public-teamcity-settings.git
 11. Открывает в Teamcity Administration -> Projects -> Root Project -> VCS Roots -> Create VCS Roots
-В repository url указываем скопированный на шаге 10 url. (В проекте должная быть хотя бы одна ветка). При подключении будет использоваться ключ, который мы добавили на шаге 7-9.
+В repository url указываем скопированный на шаге 10 url и жмем Create. (В проекте должная быть хотя бы одна ветка). При подключении будет использоваться ключ, который мы добавили на шаге 7-9.
+12. Открываем Administration -> Projects -> Root Project -> Versioned Settings. Выбираем Synchronization enabled. В списке выбираем VCS, который добавили в пункте 11. Settings Format выбираем "Kotlin". Галочка "Generate Portable DSL Scripts" включена. Жмем "Apply". Затем жмем "Commit current project settings". Если на наш сервер на github ничего не запушилось, то идем в Administration -> Projects -> Root Project, жмем справа "Actions" выбираем "Download Settings in Kotlin format" (Generate portable DSL scripts включено). Скачиваем настройки.
+13. Все файлы перекидываем в наш проект на github в папку .teamcity и пушим на сервер
+14. В Administration -> Projects -> Root Project -> Versioned Settings жмем "Load Project Settings from VCS"
+15. Открываем Intellij IDEA. Открываем в нем git проект в котором лежит .teamcity. Intellij Idea предложит импортировать проект .teamcity (В правом нижнем углу), соглашаемся. Переходим в .teamcity выполняем команду  mvn teamcity-configs:generate (Зачем это делать пока не знаю). При этом смотрим чтобы урл по которому запрашиваются либы, был таким же, какой и в pom.xml для teamcity-server. !!! По умолчанию maven блокирует http запросы. Можно увидеть в логах `maven-default-http-blocker` и использование ip: 0.0.0.0. Если вы уверены, то можно добавить url в исключения.
+Для этого нужно изменить/добавить файл ~/.m2/settings.xml.
+```xml
+<settings>
+    <mirrors>
+        <mirror>
+            <id>release-http-unblocker</id>
+            <mirrorOf>teamcity-server</mirrorOf>
+            <name></name>
+            <url>http://192.168.1.190:8111/app/dsl-plugins-repository</url>
+        </mirror>
+    </mirrors>
+</settings>
+```
+Для репозитория из pom.xml
+```xml
+    <repository>
+      <id>teamcity-server</id>
+      <url>http://192.168.1.190:8111/app/dsl-plugins-repository</url>
+      <snapshots>
+        <enabled>true</enabled>
+      </snapshots>
+    </repository>
+```
